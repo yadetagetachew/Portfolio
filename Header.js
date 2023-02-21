9c38ec127c20444dff2ef209b916a5a73c72f131
Yadeta Getachew <86324546+yadetagetachew@users.noreply.github.com>
2023-02-21 14:58:05 -0800

    added file

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+return(
+    <div className='hero d-flex justify-content-center'>
+        <img className="img-fluid me-4" src="/public/hero.png" alt='header'></img>
+    </div>
+);
+}
+
+
+export default Header;
