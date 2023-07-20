123723885ab32fbfaaabdefe2c317671cd581b38
Yadeta Getachew <86324546+yadetagetachew@users.noreply.github.com>
2023-07-20 11:48:19 -0700

    feat: title section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import './App.css';
 import Navbar from './Navbar';
 import Header from './Header';
 import Cards from './Cards';
+import Title from './Title';
 
 function App() {
   return (
     <div className='app'>
     <Navbar />
     <Header />
+    <Title 
+      title="My Projects"
+    />
     <Cards />
+    <Title 
+      title="Contact Me"
+    />
     </div>
   );
 }
---
+++ src/Title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Title(props) {
+  return (
+    <div className='title-header'>
+      <p>{props.title}</p>
+    </div>
+  )
+}
+export default Title
