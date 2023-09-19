3e224702336f8ae9243203bbf82f4968994aebc7
Yadeta Getachew <86324546+yadetagetachew@users.noreply.github.com>
2023-09-19 12:20:24 -0700

    (feat:added links to contact icons)

--- src/Contact.js
+++ src/Contact.js
@@ -2,16 +2,28 @@ import React from 'react';
 import linkedin from './media/liLogo.png'
 import email from './media/emailLogo.png'
 import resume from './media/cvLogo.png'
+import github from './media/githubLogo.png'
+import cvPdf from './documents/resume.pdf';
 
 
 const Contact = () => {
     return(
         <div>
-        <div className='contact-logo'>
-                <img src={linkedin} alt="logo" />
-                <img src={email} alt="logo" />
-                <img src={resume} alt="logo" />
-        </div>
+            <div className='contact-logo'>
+                <a href="https://linkedin.com/in/yadeta-yadi-getachew" target="_blank" rel="noreferrer" role="button">
+                    <img src={linkedin} alt="LinkedIn logo"></img>
+                </a>
+                <a href = "mailto: yadetagetachew@gmail.com">
+                    <img src={email} alt="logo" />
+                </a>
+                <a href="https://github.com/yadetagetachew" target="_blank" rel="noreferrer" role="button">
+                    <img src={github} alt="Github logo"></img>
+                </a>
+                <a href = {cvPdf} target = "_blank" rel="noreferrer" role="button">
+                    <img src={resume} alt="cv logo" />
+                </a>
+                    
+            </div>
         </div>
 
     )
